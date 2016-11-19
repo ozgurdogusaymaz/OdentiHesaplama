7591a804683fdac71a1d0b481d9f509e9d282db0
ozgurdogusaymaz <ozgurdogusaymaz@gmail.com>
2016-11-19 17:17:32 +0300

    Ödenti Hesaplama Sistemi

---
+++ odenti.Hesaplama/odenti.Hesaplama/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odenti.Hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("\nToplam Ödenti Miktarını Giriniz\t:");
+            int tom = Convert.ToInt32(Console.ReadLine());
+            
+            int GmPayi = tom * 20 / 100;
+            int IlPayi = (tom - GmPayi) * 20 / 100;
+            int Ustorg = (GmPayi + IlPayi);
+            int IlcePayi = tom - (GmPayi + IlPayi);
+
+            Console.WriteLine("\nGenel Merkez Payi\t\t:{0} TL", GmPayi);
+            Console.WriteLine("\nİl Merkezi Payi\t\t\t:{0} TL", IlPayi);
+            Console.WriteLine("\nToplam Üst Örgüt Miktarı\t:{0} TL", Ustorg);
+            Console.WriteLine("\nİlçe Merkezi Payi\t\t:{0} TL", IlcePayi);
+
+            Console.ReadKey();
+            */
+            Console.Write("\nİlçe Payını Giriniz\t\t\t:");
+            double IlcePayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nToplam Banka Kesintisini Giriniz\t:");
+            double kesinti = Convert.ToDouble(Console.ReadLine());
+
+            double tom = ((IlcePayi / 64 * 100));
+            double GPayi = (tom * 20 / 100);
+            double IPayi = ((tom - GPayi) * 20 / 100);
+            double tum = (GPayi + IPayi);
+            double tomg = (GPayi + IPayi + IlcePayi + kesinti);
+
+
+            Console.WriteLine("\nGenel Merkez Payi\t\t\t:{0} TL", GPayi);
+            Console.WriteLine("\nİl Merkezi Payi\t\t\t\t:{0} TL", IPayi);
+            Console.WriteLine("\nToplam Üst Örgüt Miktarı\t\t:{0} TL", tum);
+            Console.WriteLine("\nToplam Ödenti Miktarı\t\t\t:{0} TL", tomg);
+
+            Console.ReadKey();
+
+        }
+    }
+}
